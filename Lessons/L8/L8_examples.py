bc783397483f2e068648af249868b4628762fc94
ES Alexander <sandman.esalexander@gmail.com>
2018-12-03 01:18:48 +1000

    added recursion example to L8

--- Lessons/L8/L8_examples.py
+++ Lessons/L8/L8_examples.py
@@ -8,8 +8,17 @@
     access to the internal state of a Matrix instance.
 '''
 
-# Recursion
+# Recursion/Loops
 '''
+    Write a recursive method max_sort1 to take a list of numbers and sort it 
+    from largest to smallest. The end result should be returned, and the 
+    original list should not be modified.
+    
+    Then write max_sort2 to perform the same function as max_sort1, but using a
+    traditional (for/while) loop instead. Do not use the existing 'max' function
+    in max_sort1 or max_sort2.
+    
+    e.g. a = [1,3.5,2], b = max_sort(a) -> b = [3.5,2,1], a = [1,3.5,2]
 '''
 
 # Exception Handling/Raising
