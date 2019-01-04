d3ff73e329a9f930c8f1de9c49546ad00a5b10cf
ES Alexander <sandman.esalexander@gmail.com>
2019-01-04 11:43:02 +1000

    Attempted fix to Windows -label issue in entrycget

--- Lessons/L9/L9_example_checker.py
+++ Lessons/L9/L9_example_checker.py
@@ -99,10 +99,11 @@ class L9Tests(TestRun):
         file_cascade = None
         for menu in menubar:
             menu_type = menu.type(0)
-            if  menu_type == 'cascade':
-                label = menu.entrycget(0, 'label')
-                if label == 'File':
-                    file_cascade = menu
+            if  menu_type != 'cascade':
+                continue
+            label = menu.entrycget(0, 'label')
+            if label == 'File':
+                file_cascade = menu
         assert file_cascade is not None, "Menubar should include a 'File' menu."
 
         # check 'Clear All' command exists in 'File' menu
@@ -110,10 +111,11 @@ class L9Tests(TestRun):
         filemenu = L9Tests.get_children(file_cascade)['Menu']
         for menu_item in filemenu:
             item_type = menu_item.type(0)
-            if item_type == 'command':
-                label = menu_item.entrycget(0, 'label')
-                if label == 'Clear All':
-                    clear_command = menu_item
+            if item_type != 'command':
+                continue
+            label = menu_item.entrycget(0, 'label')
+            if label == 'Clear All':
+                clear_command = menu_item
         assert clear_command is not None, \
                "'File' menu should include a 'Clear All' command."
 
