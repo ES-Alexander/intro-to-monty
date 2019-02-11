f508b36217499daaa663c033665633070783b8fa
ES Alexander <sandman.esalexander@gmail.com>
2019-02-11 13:41:10 +1000

    Modified TestRun in-file license, exempted from general license.

--- Lessons/TestRun.py
+++ Lessons/TestRun.py
@@ -9,13 +9,29 @@
 # Author: ES Alexander                                                         #
 # Released: 2018-12-26                                                         #
 #                                                                              #
-# Feel free to use and modify as you wish, but keep this header and add a note #
-#   specifying how it has been modified from the original, the last date it    #
-#   was modified, and who modified it.                                         #
+# This module is exempt from any external license it is provided with, and is  #
+#   instead provided under the modified MIT license below:                     #
 #                                                                              #
-# Do not attempt to sell this testing module. If planning to use commercially, #
-#   or sell tests derived from the module, please contact the original author  #
-#   at 'sandman.esalexander@gmail.com'.                                        #
+# Copyright 2019 ES Alexander                                                  #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person obtaining a copy #
+# of this software and associated documentation files (the "Software"), to     #
+# deal in the Software without restriction, including without limitation the   #
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or  #
+# sell copies of the Software, and to permit persons to whom the Software is   #
+# furnished to do so, subject to the following conditions:                     #
+#                                                                              #
+# The above header, the above copyright notice, this permission notice, and a  #
+# note specifying how the module has been modified from the original shall be  #
+# included in all copies or substantial portions of the Software.              #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
+# IN THE SOFTWARE.                                                             #
 #                                                                              #
 ###--------------------------------------------------------------------------###
 #                                                                              #
